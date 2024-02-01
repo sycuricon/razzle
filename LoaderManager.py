9158af36a8c72735bcf3774432bc477f456d4c34
zhouyangye1076 <1076192792@QQ.com>
2024-02-01 16:45:16 +0800

    use Loader Manager to generate ld

---
+++ LoaderManager.py
@@ -0,0 +1,34 @@
+def section_sort(item):
+    return item[0]
+
+class LoaderManager:
+    def __init__(self):
+        self.section=[]
+
+    def append_section_list(self,section_list):
+        for name,begin in section_list:
+            self.append_section((begin,name))
+
+    def append_section(self,name,begin):
+        self.section.append((begin,name))
+
+    def generate_ld(self,ld_name):
+        with open(ld_name,"wt") as f:
+            section_order=sorted(self.section,key=section_sort)
+            f.write('OUTPUT_ARCH( "riscv" )\n')
+            f.write('ENTRY(_start)\n')
+            f.write('SECTIONS\n')
+            f.write('{\n')
+            for begin,name in section_order:
+                f.write('\t'+'. = '+hex(begin)+';\n')
+                f.write('\t'+name+' = { *('+name+') }\n')
+            f.write('}\n')
+
+if __name__ == "__main__":
+    loader=LoaderManager()
+    loader.append_section(".text",0x10000)
+    loader.append_section(".data",0x20000)
+    loader.append_section(".tmp",0x0)
+    loader.append_section(".here",0x20)
+    loader.generate_ld("link.ld")
+            
