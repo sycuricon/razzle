a7ef37e9008fc90fb378a56bc49c0e845c222ff6
zhouyangye1076 <1076192792@QQ.com>
2024-02-18 22:49:53 +0800

    change debug to 2 memory

--- debug/spike_sim.cc
+++ debug/spike_sim.cc
@@ -5,10 +5,9 @@ int main(){
     config_t cfg;
     cfg.verbose = true;
     cfg.isa = "rv64gc_zicsr_zicntr";
-    cfg.boot_addr = 0x10000;
+    cfg.boot_addr = 0x20000;
     cfg.elffiles = std::vector<std::string> {
         "/home/zyy/starship-parafuzz/build/firmware/rvsnap/init.elf",
-        "/home/zyy/starship-parafuzz/build/fuzz_code/reg_init.elf",
         "/home/zyy/starship-parafuzz/build/fuzz_code/Testbench"
     };
     cfg.mem_layout = std::vector<mem_cfg_t> {
