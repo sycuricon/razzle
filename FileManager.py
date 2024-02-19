597252905cd5a2cc6ed2c1dc74fb8c6a572794bd
zhouyangye1076 <1076192792@QQ.com>
2024-02-19 21:06:25 +0800

    add filemanager

--- FileManager.py
+++ FileManager.py
@@ -1,25 +1,27 @@
 from SectionManager import *
 from Utils import *
 
-class FuzzPage(Page):
+class FilePage(Page):
     def __init__(self,vaddr,paddr,flag):
         super().__init__(vaddr,paddr,flag)
 
     def generate_asm(self,is_variant):
         pass
 
-class FuzzSection(Section):
+class FileSection(Section):
     def __init__(self,name,length,section_label=[],pages=[]):
         super().__init__(name,length,section_label,pages)
 
-class FuzzManager(SectionManager):
+class FileManager(SectionManager):
     def __init__(self,config):
         super().__init__(config)
+        self.folder=config["folder"]
 
     def file_generate(self,path,name):
         self._generate_pages()
         self._generate_section_list()
         filename=[]
+        print(self.folder)
         for folder in self.folder:
             os.system("cp "+folder+'/* '+path)
             files=list(filter(lambda filename:filename.endswith(('.S','.c')),os.listdir(folder)))
