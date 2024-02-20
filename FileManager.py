24685738a30beee26ffcdba3c60b61702202b023
zhouyangye1076 <1076192792@QQ.com>
2024-02-20 15:22:56 +0800

    fix file cp bug

--- FileManager.py
+++ FileManager.py
@@ -22,13 +22,13 @@ class FileManager(SectionManager):
         self._generate_pages()
         self._generate_section_list()
         filename=[]
-        print(self.folder)
+
         for folder in self.folder:
             os.system("cp "+folder+'/* '+path)
             files=list(filter(lambda filename:filename.endswith(('.S','.c')),os.listdir(folder)))
             filename.extend(files)
         for file in self.file:
-            os.system("cp "+file+'/* '+path)
+            os.system("cp "+file+' '+path)
             filename.append(os.path.basename(file))
         for i in range(len(filename)):
             filename[i]=os.path.join(path,filename[i])
