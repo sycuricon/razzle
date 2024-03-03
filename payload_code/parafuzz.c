149dae73aaa7ee83f7e3de8cdd60f4a4303adf71
zhouyangye1076 <1076192792@QQ.com>
2024-03-03 21:13:59 +0800

    add poc_code folder and include folder

--- payload_code/parafuzz.c
+++ payload_code/parafuzz.c
@@ -75,24 +75,3 @@ void setup_pmp() {
   cfg0 = PMP_NAPOT | PMP_L;
   write_csr(pmpcfg0, cfg0);
 }
-
-__attribute__((optimize("align-functions=64")))
-__attribute__((section (".poc.text")))
-uint64_t access_time(uint8_t* base, int index, int offset) {
-  INFO_LEAK_START;
-  uint64_t time1 = 0, time2;
-  uint8_t *addr = base + index*CACHE_BLOCK + offset;
-  int junk = 0;
-  time1 = read_csr(cycle);
-  junk = *(addr);
-  time2 = read_csr(cycle);
-  INFO_LEAK_END;
-
-  // printf(
-  //   "[*] Access time for array[%4d*%4d~%4d]@%p=%ld, takes %ld CPU cycles\n", 
-  //     index, CACHE_BLOCK, offset,
-  //     addr, junk, 
-  //     time2-time1);
-
-  return time2-time1;
-}
---
+++ poc_code/poc.c
@@ -0,0 +1,21 @@
+#include "parafuzz.h"
+__attribute__((optimize("align-functions=64")))
+__attribute__((section (".poc.text")))
+uint64_t access_time(uint8_t* base, int index, int offset) {
+  INFO_LEAK_START;
+  uint64_t time1 = 0, time2;
+  uint8_t *addr = base + index*CACHE_BLOCK + offset;
+  int junk = 0;
+  time1 = read_csr(cycle);
+  junk = *(addr);
+  time2 = read_csr(cycle);
+  INFO_LEAK_END;
+
+  // printf(
+  //   "[*] Access time for array[%4d*%4d~%4d]@%p=%ld, takes %ld CPU cycles\n", 
+  //     index, CACHE_BLOCK, offset,
+  //     addr, junk, 
+  //     time2-time1);
+
+  return time2-time1;
+}
