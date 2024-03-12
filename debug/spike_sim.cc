8c4c4de818b6364014f35e89572c20663df0e3e7
zhouyangye1076 <1076192792@QQ.com>
2024-03-12 16:46:36 +0800

    add sim target arg for sim_spike

--- debug/spike_sim.cc
+++ debug/spike_sim.cc
@@ -1,20 +1,26 @@
 #include "cj.h"
 #include <stdio.h>
 
-int main(){
+int main(int argc, const char* argv[]){
     config_t cfg;
     cfg.verbose = true;
     cfg.isa = "rv64gc_zicsr_zicntr";
     cfg.boot_addr = 0x80000000;
-    cfg.elffiles = std::vector<std::string> {
-        "/home/zyy/starship-parafuzz/build/fuzz_code/Testbench"
-    };
+    if(argc==1){
+        cfg.elffiles = std::vector<std::string> {
+            "../../../build/fuzz_code/Testbench"
+        };
+    }else{
+        cfg.elffiles = std::vector<std::string> {
+            argv[1]
+        };
+    }
     cfg.mem_layout = std::vector<mem_cfg_t> {
         mem_cfg_t(0x80000000UL, 0x80000000UL),
     };
     cfg.logfile=fopen("log","wt");
     cosim_cj_t* simulator = new cosim_cj_t(cfg);
-    for(int i=0;i<1000;i++){
+    for(int i=0;i<8000;i++){
         simulator->cosim_commit_stage(0,0,0,false);
         // puts(">\n");
         // getchar();
