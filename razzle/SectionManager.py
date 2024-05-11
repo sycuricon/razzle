e953696a472431c69a38873bee5440c119550c35
zhouyangye1076 <1076192792@QQ.com>
2024-05-11 13:17:13 +0800

    fix path bug in frame_file_list

--- razzle/SectionManager.py
+++ razzle/SectionManager.py
@@ -131,7 +131,7 @@ class SectionManager:
         with open(filename, "wt") as f:
             self._write_headers(f)
             self._write_sections(f)
-        self.dut_file_list = [f"{filename}"]
+        self.dut_file_list = [f"$OUTPUT_PATH/{name}"]
 
     def file_generate(self, path, name):
         self._generate_sections()
