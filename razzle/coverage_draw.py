d160cb78d035afd69748fd82f516100a0ecd86b4
zhouyangye1076 <1076192792@QQ.com>
2024-05-31 12:20:22 +0800

    add coverage state axvline

--- razzle/coverage_draw.py
+++ razzle/coverage_draw.py
@@ -4,11 +4,30 @@ import matplotlib.pyplot as plt
 
 def coverage_draw(curve_fuzz):
     cov_inc = [0]
+    trigger_mutate = [0]
+    access_mutate = []
+    leak_mutate = []
     for line in open(curve_fuzz, "rt"):
         if line.startswith("inc coverage:"):
             cov_inc.append(int(line.split()[-1])+cov_inc[-1])
+        elif line.startswith("state switch:"):
+            _, _, state1, _, state2 = line.split()
+            if state1 == '[FuzzFSM.MUTATE_LEAK]' and state2 == '[FuzzFSM.IDLE]':
+                trigger_mutate.append(len(cov_inc))
+            elif state1 == '[FuzzFSM.ACCUMULATE]' and state2 == '[FuzzFSM.MUTATE_ACCESS]':
+                access_mutate.append(len(cov_inc))
+            elif state1 == '[FuzzFSM.MUTATE_LEAK]' and state2 == '[FuzzFSM.ACCUMULATE]':
+                leak_mutate.append(len(cov_inc))
     curve_folder = os.path.dirname(curve_fuzz)
     plt.plot(cov_inc)
+
+    for line in leak_mutate:
+        plt.axvline(line, color='red')
+    for line in access_mutate:
+        plt.axvline(line, color='green')
+    for line in trigger_mutate:
+        plt.axvline(line, color='blue')
+
     plt.savefig(os.path.join(curve_folder, 'coverage.png'))
 
 if __name__ == "__main__":
