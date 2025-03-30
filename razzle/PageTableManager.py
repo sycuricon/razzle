9496d135c1950b88fdb499dfb96bb9fa1da57c40
zhouyangye1076 <1076192792@QQ.com>
2025-03-30 11:57:41 +0800

    new page table construct with more memory

--- razzle/PageTableManager.py
+++ razzle/PageTableManager.py
@@ -4,28 +4,34 @@ import hjson
 import random
 
 class PageTablePage:
-    def __init__(self, xLen, pg_level):
-        self.pg_level = pg_level
+    def __init__(self, xLen, paddr, pg_level):
         self.entry_num = 512 if xLen == 64 else 1024
         self.index_width = 9 if xLen == 64 else 10
         self.entry_byte = 8 if xLen == 64 else 4
         self.content = [0] * self.entry_num
-        self.stage_array = [0] * self.entry_num
+        self.page_array = [None] * self.entry_num
         self.vaddr_array = [0] * self.entry_num
+        self.paddr = paddr
+        self.pg_level = pg_level
 
     def is_valid(self, entry_num):
         return (self.content[entry_num] & Flag.V) != 0
+    
+    def get_next_table(self, entry_num):
+        return self.page_array[entry_num]
 
-    def entry_stage(self, entry_num):
-        return self.stage_array[entry_num]
-
-    def fill_entry(self, entry_num, paddr, flag, stage, vaddr):
+    def fill_entry(self, entry_num, paddr, flag, vaddr, page_table):
         self.content[entry_num] = ((paddr >> 12) << 10) | flag
-        self.stage_array[entry_num] = stage
+        self.page_array[entry_num] = page_table
         self.vaddr_array[entry_num] = vaddr
 
     def generate_asm(self):
         write_lines = []
+        write_lines.extend(
+            Asmer.label_inst(
+                f'paddr_{hex(self.paddr)}'
+            )
+        )
         empty_entry = 0
         for i in range(self.entry_num):
             entry_value = self.content[i]
@@ -33,15 +39,15 @@ class PageTablePage:
                 if empty_entry != 0:
                     write_lines.extend(Asmer.space_inst(self.entry_byte * empty_entry))
                     empty_entry = 0
-                if self.stage_array[i] == self.pg_level - 1:
-                    write_lines.extend(
-                        Asmer.label_inst(
-                            "vaddr_"
-                            + hex(self.vaddr_array[i])
-                            + "_paddr_"
-                            + hex((self.content[i] >> 10) << 12)
-                        )
+                mask = (1 << (self.index_width * (self.pg_level - self.stage_num - 1) + 12)) - 1
+                write_lines.extend(
+                    Asmer.label_inst(
+                        "vaddr_"
+                        + hex(self.vaddr_array[i] | mask)
+                        + "_paddr_"
+                        + hex((self.content[i] >> 10) << 12)
                     )
+                )
                 if self.entry_byte == 8:
                     write_lines.extend(Asmer.quad_inst(entry_value))
                 else:
@@ -76,50 +82,43 @@ class PageTableManager(SectionManager):
         self.pg_level = config["pg_level"]
         self.page_tables = []
         self.pgtlb_flag = []
-        assert (
-            self.memory_bound[0][1] - self.memory_bound[0][0]
-            >= self.pg_level * Page.size
-        )
-        paddrs = list(
-            range(
+        self.free_page_tables = [
+            PageTablePage(self.xLen, paddr, self.pg_level)
+            for paddr in range(
                 self.memory_bound[0][0],
-                self.memory_bound[0][0] + self.pg_level * Page.size,
+                self.memory_bound[0][1],
                 Page.size,
             )
-        )
-        self.page_tables = [
-            PageTablePage(self.xLen, self.pg_level) for i in range(self.pg_level)
         ]
-        self.pgtlb_paddr = paddrs[1:] + [0]
-        self.pgtlb_flag = [Flag.V] * (self.pg_level - 1) + [0]
+        self.root_page_tables = self._alloc_free_page(0)
         self.index_width = 9 if self.xLen == 64 else 10
+    
+    def _alloc_free_page(self, stage_num):
+        if len(self.free_page_tables) == 0:
+            raise Exception("no free page table in free_page_tables")
+        self.page_tables.append(self.free_page_tables.pop(0))
+        self.page_tables[-1].stage_num = stage_num
+        return self.page_tables[-1]
 
     def _register_page(self, vaddr, paddr, flag):
-        self.pgtlb_paddr[-1] = paddr
-        self.pgtlb_flag[-1] = flag | Flag.A | Flag.D | Flag.V
+        pgtlb_flag = flag | Flag.A | Flag.D | Flag.V
 
         old_vaddr = vaddr
         vaddr >>= 12
         mask = (1 << self.index_width) - 1
+        page_table = self.root_page_tables
         for i in range(self.pg_level):
-            entry_num = vaddr & mask
-            vaddr >>= self.index_width
-            stage = self.pg_level - i - 1
-            if (
-                stage == self.pg_level - 1
-                and not self.page_tables[stage].is_valid(entry_num)
-                or stage != self.pg_level - 1
-                and self.page_tables[stage].entry_stage(entry_num) != self.pg_level - 1
-            ):
-                self.page_tables[stage].fill_entry(
-                    entry_num,
-                    self.pgtlb_paddr[stage],
-                    self.pgtlb_flag[stage],
-                    stage,
-                    old_vaddr,
-                )
-            else:
-                raise "virtual address is conflicted"
+            offset = self.index_width * (self.pg_level - i - 1)
+            entry_num = (vaddr >> offset) & mask
+            if not page_table.is_valid(entry_num):
+                next_paddr = paddr
+                next_page_table = None
+                if i != self.pg_level - 1:
+                    next_page_table = self._alloc_free_page(i + 1)
+                    next_paddr = next_page_table.paddr
+                page_table.fill_entry(entry_num, next_paddr, flag, old_vaddr, next_page_table)
+            if i != self.pg_level - 1:
+                page_table = page_table.get_next_table(entry_num)
     
     def load_config(self, filename):
         def str2int(value):
@@ -178,16 +177,16 @@ class PageTableManager(SectionManager):
             for offset in range(0, length, Page.size):
                 vaddr_offset = vaddr + offset
                 paddr_offset = paddr + offset
-                # vaddr_virtual_offset = vaddr_offset + 0xFFFFFFFFFFF00000
-                self._register_page(vaddr_offset, paddr_offset, flag)
-                # self._register_page(vaddr_virtual_offset, paddr_offset, flag & ~Flag.U)
+                vaddr_virtual_offset = vaddr_offset + 0xFFFFFFFF00000000
+                self._register_page(vaddr_offset, paddr_offset, flag | Flag.U)
+                self._register_page(vaddr_virtual_offset, paddr_offset, flag & ~Flag.U)
 
     def _generate_sections(self):
         self.section["pagetable"] = PageTableSection(
             ".pagetable", self.pg_level, self.page_tables
         )
-        self.section["pagetable"].add_global_label(["vaddr_0x4000_paddr_0x80004000"])
-        self.section["pagetable"].add_global_label(["vaddr_0xfffffffffff04000_paddr_0x80004000"])
+        self.section["pagetable"].add_global_label(["vaddr_0x80007000_paddr_0x80007000"])
+        self.section["pagetable"].add_global_label(["vaddr_0xffffffff80004000_paddr_0x80007000"])
 
     def _distribute_address(self):
         self.section["pagetable"].get_bound(
