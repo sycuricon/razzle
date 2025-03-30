9fcfe8422435e6787495de1ad5cfdf30081da602
zhouyangye1076 <1076192792@QQ.com>
2025-03-30 14:13:06 +0800

    add trap and tohost

---
+++ razzle/template/specgo_trans/template.h
@@ -0,0 +1,10 @@
+#ifndef _SPECGO_TEMPLATE_H_
+#define _SPECGO_TEMPLATE_H_
+
+#define sim_exit                                \
+    la a0, tohost;                              \
+    nop;                                        \
+1:  sd gp, 0(a0);                            \
+    j 1b;
+
+#endif
